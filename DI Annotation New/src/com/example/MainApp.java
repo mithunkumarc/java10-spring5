f0a04d5e075533aa1ab16755345f5d32001490d2
mithunkumarc <mithunchannappa@gmail.com>
2018-07-26 14:28:37 +0530

    added comment
    Spring Configuration annotation indicates that the class has @Bean definition methods. 
    So Spring container can process the class and generate Spring Beans to be used in the application.

--- DI Annotation New/src/com/example/MainApp.java
+++ DI Annotation New/src/com/example/MainApp.java
@@ -1,7 +1,11 @@
 package com.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+/* 
+	@Configuration in AppConfig class
+	Spring Configuration annotation indicates that the class has @Bean definition methods. 
+   	So Spring container can process the class and generate Spring Beans to be used in the application.
+*/
 public class MainApp {
 	   public static void main(String[] args) {
 	      AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
