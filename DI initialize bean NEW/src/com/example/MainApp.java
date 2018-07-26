f2776ea4a66462d10a13ccaceb192c993af2f7d1
mithunkumarc <mithunchannappa@gmail.com>
2018-07-26 13:10:52 +0530

    Di initialize properties of bean NEW

---
+++ DI initialize bean NEW/src/com/example/MainApp.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+	   public static void main(String[] args) {
+	      AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	      Company company = context.getBean(Company.class);
+	      company.showEmployeeInfo();
+	      context.close();
+	   }
+	}
