fb6abd98f51dc1efb0cdd7fa967f6cb1609efe16
mithunkumarc <mithunchannappa@gmail.com>
2018-08-01 16:04:54 +0530

    Update SecurityConfiguration.java

--- session+basicAuth/src/com/example/config/SecurityConfiguration.java
+++ session+basicAuth/src/com/example/config/SecurityConfiguration.java
@@ -15,6 +15,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
 		System.out.println("this is inside configureGlobalSecurity");
+		//below code is for inmemory , you can auth against database, see last comment 
 		auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("USER");
 		auth.inMemoryAuthentication().withUser("admin").password("root123").roles("ADMIN");
 		auth.inMemoryAuthentication().withUser("dba").password("root123").roles("ADMIN","DBA");
@@ -22,13 +23,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	  System.out.println("thies is inside configure method where http authorize request");
-	  http.authorizeRequests()
+	  	//below line executes one time
+		System.out.println("thies is inside configure method where http authorize request");
+	  	//below method called for every login request and authenticated against inmemory or database
+		http.authorizeRequests()
 	  	.antMatchers("/", "/home").permitAll()
 	  	.antMatchers("/admin/**").access("hasRole('ADMIN')")
 	  	.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
 	  	.and().formLogin().loginPage("/login")
 	  	.usernameParameter("ssoId").passwordParameter("password")
 	  	.and().exceptionHandling().accessDeniedPage("/Access_Denied");
+		
+		//admin, db page cannot be accessed with out login authentication, delegate filter will handle this
+	  //ssoId and password are keys for username and password in login.jsp
 	}
 }
+
+/*
+   auth.jdbcAuthentication().dataSource(dataSource)
+  .usersByUsernameQuery(
+   "select username,password, enabled from users where username=?")
+  .authoritiesByUsernameQuery(
+   "select username, role from user_roles where username=?");
+*/
+
